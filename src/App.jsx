19971e020257d9c4b86fcb596a039fe865e6fbb4
Mubeeb <mohammedmubeen002@gmail.com>
2025-08-08 19:37:45 +0530

    First React project - Hello Card

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div style={{
+      fontFamily: "sans-serif",
+      textAlign: "center",
+      marginTop: "50px",
+    }}>
+      <h1>Hello World!</h1>
+      <p>My first React component 🚀</p>
+    </div>
+  );
+}
+
+export default App;
